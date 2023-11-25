04a4dbf90031d3f3c011025a73cf0f4408c76f57
ivan <ivan@ivan-manjaro-1.home>
2023-11-25 01:52:52 +0100

    Ajout de la page succès

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import io.javalin.Javalin;
 import io.javalin.http.Context;
-import io.javalin.http.staticfiles.Location;
+import io.javalin.http.HttpStatus;
+import io.javalin.rendering.JavalinRenderer;
+import io.javalin.rendering.template.JavalinMustache;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class Main {
     private static String BASE_FOLDER = null;
@@ -11,13 +15,20 @@ public class Main {
     public static void main(String[] args) {
         parseBaseFolder(args);
 
-        Javalin app = Javalin.create(config ->
-                        config.staticFiles.add("/view", Location.CLASSPATH))
-                .start(7070);
+        Javalin app = Javalin.create().start(7070);
+
+        JavalinRenderer.register(new JavalinMustache(), ".hbs");
+
+        app.get("/", ctx -> ctx.render("src/main/hbs/index.hbs"));
+        app.get("/success", ctx -> ctx.render("src/main/hbs/index.hbs", model("message", """
+                    <div class="notification is-success mt-6">
+                      Téléchargement en cours ! 👌\s
+                    </div>
+                """)));
 
         app.post("/download", ctx -> {
             ctx.future(() -> downloadAsync(ctx));
-            ctx.redirect("/");
+            ctx.redirect("/success");
         });
     }
 
